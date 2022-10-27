fd255a2aa8c034b7a05a4df90b268862b56a510a
Nitin Pandey <npandey@deqode.com>
2022-10-27 19:36:18 +0530

    Bugfix: Home screen

--- src/Screens/ChatScreen.jsx
+++ src/Screens/ChatScreen.jsx
@@ -1,65 +1,103 @@
-import {signOut} from 'firebase/auth';
-import React, {useState} from 'react';
+import React, {useCallback, useState, useLayoutEffect, useEffect} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 
-import {GiftedChat, Bubble} from 'react-native-gifted-chat';
+import auth from '@react-native-firebase/auth';
+import {
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  orderBy,
+  onSnapshot,
+  db,
+} from 'firebase/firestore';
+import {GiftedChat} from 'react-native-gifted-chat';
+import firestore from '@react-native-firebase/firestore';
 
-export default function ChatScreen() {
-  const [messages, setMessages] = useState([
-    /**
-     * Mock message data
-     */
-    // example of system message
-    {
-      _id: 0,
-      text: 'New room created.',
-      createdAt: new Date().getTime(),
-      system: true,
-    },
-    // example of chat message
-    {
-      _id: 1,
-      text: 'Henlo!',
-      createdAt: new Date().getTime(),
-      user: {
-        _id: 2,
-        name: 'Test User',
-      },
-    },
-  ]);
+const ChatScreen = ({navigation}) => {
+  const [messages, setMessages] = useState([]);
 
-  function renderBubble(props) {
-    return (
-      // Step 3: return the component
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          right: {
-            // Here is the color change
-            backgroundColor: '#6646ee',
-          },
-        }}
-        textStyle={{
-          right: {
-            color: '#fff',
-          },
-        }}
-      />
-    );
-  }
-  function handleSend(newMessage = []) {
-    setMessages(GiftedChat.append(messages, newMessage));
-  }
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection('chats')
+      .onSnapshot(querySnapshot => {
+        const messages = [];
+
+        querySnapshot.forEach(documentSnapshot => {
+          console.log(documentSnapshot, 'sdjsdfjhdsfjsjfsdjhasd');
+          messages.push({
+            ...documentSnapshot.data(),
+            Id: documentSnapshot.data().Id,
+            createdAt: documentSnapshot.data().createdAt.toDate(),
+            Text: documentSnapshot.data().Text,
+            user: documentSnapshot.data().user,
+          });
+        });
+
+        setMessages(messages);
+      });
+
+    // Unsubscribe from events when no longer in use
+    return () => subscriber();
+  }, []);
+
+  const signOutNow = () => {
+    auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  };
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      // headerLeft: () => (
+      //   <View style={{marginLeft: 20}}>
+      //     <Image
+      //       source={{
+      //         uri: firestore.collection('chats').doc()
+      //       }}
+      //     />
+      //   </View>
+      // ),
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            marginRight: 10,
+          }}
+          onPress={signOutNow}>
+          <Text>logout</Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
+  const onSend = useCallback((messages = []) => {
+    const {Id, createdAt, Text, user} = messages[0];
+
+    debugger;
+    firestore()
+      .collection('chats')
+      .add({
+        Id: Id,
+        createdAt: createdAt,
+        Text: Text,
+        user: user,
+      })
+      .then(() => {
+        console.log('User added!');
+      });
+  }, []);
 
   return (
     <GiftedChat
       messages={messages}
-      onSend={newMessage => handleSend(newMessage)}
-      user={{Id: 1, Name: 'User Test'}}
-      renderBubble={renderBubble}
-      placeholder="Type your message here..."
-      showUserAvatar
-      alwaysShowSend
-      scrollToBottom
+      showAvatarForEveryMessage={true}
+      onSend={messages => onSend(messages)}
+      user={{
+        Id: auth?.currentUser?.email,
+        name: auth?.currentUser?.displayName,
+        avatar: auth?.currentUser?.photoURL,
+      }}
     />
   );
-}
+};
+
+export default ChatScreen;
--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -1,61 +1,65 @@
 import React, {useState, useEffect} from 'react';
-import {View, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
+import {View, StyleSheet, FlatList, TouchableOpacity, Text} from 'react-native';
 import {List, Divider} from 'react-native-paper';
 import firestore from '@react-native-firebase/firestore';
-import Loader from '../Components/Loader';
+import {QuerySnapshot} from 'firebase/firestore';
 
 export default function HomeScreen({navigation}) {
   const [threads, setThreads] = useState([]);
-  const [loading, setLoading] = useState(true);
 
+  /**
+   * Fetch threads from Firestore
+   */
   useEffect(() => {
+    //const unsubscribe = await firestore().collection('Users').get();
     const unsubscribe = firestore()
-      .collection('THREADS')
-      // .orderBy('latestMessage.createdAt', 'desc')
-      .onSnapshot(querySnapshot => {
-        const threads = querySnapshot.docs.map(documentSnapshot => {
-          return {
-            _id: documentSnapshot.id,
-            // give defaults
-            name: '',
-            ...documentSnapshot.data(),
-          };
-        });
-
-        setThreads(threads);
-
-        if (loading) {
-          setLoading(false);
-        }
+      .collection('chats')
+      .get()
+      .then(querySnapshot => {
+        let data = querySnapshot.docs.map(snapShot => snapShot.data());
+        console.log(data, 'showData');
+        setThreads(data);
       });
+    // setThreads([...data]);
 
-    /**
-     * unsubscribe listener
-     */
+    // .onSnapshot(querySnapshot => {
+    //   console.log(querySnapshot.docs, 'datashow');
+    //   const threads = querySnapshot.docs.data.map(documentSnapshot => {
+    //     return {
+    //       _id: documentSnapshot.data().Id,
+    //       // give defaults
+    //       name: documentSnapshot.data().user.name,
+    //       ...documentSnapshot.data(),
+    //     };
+    //   });
+    //   console.log(querySnapshot, 'after');
+    //   // debugger;
+
+    //   setThreads(threads);
+    //   console.log('threads==', threads);
+    // });
+
     return () => unsubscribe();
   }, []);
-
-  if (loading) {
-    return <Loader />;
-  }
 
   return (
     <View style={styles.container}>
       <FlatList
         data={threads}
-        keyExtractor={item => item._id}
+        keyExtractor={item => item.Id}
         ItemSeparatorComponent={() => <Divider />}
         renderItem={({item}) => (
           <TouchableOpacity
             onPress={() => navigation.navigate('HomePage', {thread: item})}>
             <List.Item
-              title={item.Name}
-              description="Item description"
+              title={item.user.name}
+              description="Tap to Chat"
               titleNumberOfLines={1}
               titleStyle={styles.listTitle}
               descriptionStyle={styles.listDescription}
               descriptionNumberOfLines={1}
             />
+            <Text>{console.log(item, 'showItem')}</Text>
           </TouchableOpacity>
         )}
       />
